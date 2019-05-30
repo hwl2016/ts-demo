085e65f9ee8be6bc077ba6304ee480c2207e0b75
wenlong.hu <wenlong.hu@ejabhost1>
2019-05-30 17:03:08 +0800

    haha

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-import { Son, Api, Clock, Employee } from './base'
+// import { Son, Api, Clock, Employee } from './base'
 
 // const son = new Son('Mike', 28)
 // son.say('Hello')
 // Api.test()
 
-const clock = new Clock(16, 3)
-clock.setTime('2019-05-25')
-const t = clock.getTime()
-console.log(t)
+// const clock = new Clock(16, 3)
+// clock.setTime('2019-05-25')
+// const t = clock.getTime()
+// console.log(t)
+
+// const { spawn } = require('child_process');
+const spawn = require('cross-spawn');
+// const cmd = spawn('ls', ['-lh', '/usr']);
+const cmd = spawn('git', ['add', '.'])
+
+cmd.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+cmd.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+cmd.on('close', (code) => {
+  console.log(`子进程退出码：${code}`);
+});

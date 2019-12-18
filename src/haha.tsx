eeba357806305313b50bb9f4e97b799c9c0d360c
wenlong.hu <wenlong.hu@ejabhost1>
2019-12-18 13:02:39 +0800

    SSS

---
+++ src/haha.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactPullLoad, {STATS} from 'react-pullload'
+const h = React.createElement;
+
+export default class Haha extends React.Component {
+    render() {
+        return (
+            <ReactPullLoad>
+                <div>haha</div>
+            </ReactPullLoad>
+        )
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -64,8 +64,8 @@ let [first, second] = input;
 
 [first, second] = [second, first];
 
-console.log(first); // outputs 1
-console.log(second); // outputs 2
+// console.log(first); // outputs 1
+// console.log(second); // outputs 2
 
 function f([first, second]: [number, number]) {
     console.log(first);
@@ -79,9 +79,9 @@ function abc({a, b = 0}: C = {a: ""}): void {
     console.log(a, b);
 }
 
-abc()   // OK
-abc({a: 'haha'})   // OK
-abc({a: 'haha', b: 10})   // OK
+// abc()   // OK
+// abc({a: 'haha'})   // OK
+// abc({a: 'haha', b: 10})   // OK
 // abc({})   // Error
 
 class D {
@@ -93,4 +93,208 @@ let d = new D();
 let clone = { ...d };
 // clone.p; // ok
 // clone.m(); // error!
-console.log('clone => ', clone);
+// console.log('clone => ', clone);
+
+
+
+abstract class Department {
+    constructor(public name: string) {
+    }
+    printName(): void {
+        console.log('Department name: ' + this.name);
+    }
+    abstract printMeeting(): void; // 必须在派生类中实现
+}
+class AccountingDepartment extends Department {
+    constructor() {
+        super('Accounting and Auditing'); // 在派生类的构造函数中必须调用 super()
+    }
+    printMeeting(): void {
+        console.log('The Accounting Department meets each Monday at 10am.');
+    }
+    generateReports(): void {
+        console.log('Generating accounting reports...');
+    }
+}
+
+let department: Department; // 允许创建一个对抽象类型的引用
+// department = new Department(); // 错误: 不能创建一个抽象类的实例
+// department = new AccountingDepartment(); // 允许对一个抽象子类进行实例化和赋值
+// department.printName();
+// department.printMeeting();
+// department.generateReports(); // 错误: 方法在声明的抽象类中不存在
+
+class Greeter {
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
+
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greet());
+
+let greeterMaker: typeof Greeter = Greeter;
+greeterMaker.standardGreeting = "Hey there!";
+
+let greeter2: Greeter = new greeterMaker();
+greeter2.greeting = 'Michael';
+console.log(greeter2.greet());
+
+// class Point {
+//     x: number;
+//     y: number;
+// }
+// interface Point3d extends Point {
+//     z: number;
+// }
+// let point3d: Point3d = {x: 1, y: 2, z: 3};
+
+
+function buildName(firstName: string, lastName = "Smith") {
+    return firstName + " " + lastName;
+}
+
+let result1 = buildName("Bob");                  // works correctly now, returns "Bob Smith"
+let result2 = buildName("Bob", undefined);       // still works, also returns "Bob Smith"
+// let result3 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+let result4 = buildName("Bob", "Adams");
+
+interface Card {
+    suit: string;
+    card: number;
+}
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
+let deck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function(this: Deck) {
+        // const _this = this;
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);  // Now we know it has a .length property, so no more error
+    return arg;
+}
+
+loggingIdentity({a: 123, length: 1});
+
+
+// 在泛型约束中使用类型参数
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+// let x: {[propName: string]: number} = { a: 1, b: 2, c: 3, d: 4 };
+
+// getProperty(x, "a"); // okay
+// getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
+
+
+// 在泛型里使用类类型
+function create<T>(c: {new(): T; }): T {
+    return new c();
+}
+
+
+class BeeKeeper {
+    hasMask: boolean;
+}
+
+class ZooKeeper {
+    nametag: string;
+}
+
+class Animal {
+    numLegs: number;
+}
+
+class Bee extends Animal {
+    keeper: BeeKeeper;
+}
+
+class Lion extends Animal {
+    keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: {new(): A;}): A {
+    return new c();
+}
+
+createInstance(Lion).keeper.nametag = '1';  // typechecks!  string
+createInstance(Bee).keeper.hasMask = true;   // typechecks!  boolean
+
+
+enum Status { Ready, Waiting };
+enum Color { Red, Blue, Green };
+
+let hey = Status.Ready;
+// hey = Color.Green;  // Error
+
+
+interface Bird {
+    fly();
+    layEggs();
+}
+
+interface Fish {
+    swim();
+    layEggs();
+}
+
+function getSmallPet(): Fish | Bird {
+    const Haha: (Fish | Bird) =  {
+        layEggs() {},
+        swim() {},
+        fly() {}
+    }
+    return Haha;
+}
+
+let pet = getSmallPet();
+pet.layEggs(); // okay
+// pet.swim();    // errors
+
+// if ((<Fish>pet).swim) {
+//     (pet as Fish).swim();
+// } else {
+//     (<Bird>pet).fly();
+// }
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (<Fish>pet).swim !== undefined;
+}
+
+if (isFish(pet)) {
+    pet.swim();
+}else {
+    pet.fly();
+}
